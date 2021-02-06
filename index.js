54b8439a0199255cef5acf06c7c5acf965872b0e
Asher Mitilinakis <ashermitilinakis@Ashers-Air.local>
2021-02-06 12:41:49 +0000

    added product page js

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ let tl = new TimelineMax({ onUpdate: updatePercentage });
 let tl2 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 
-tl.from('blockquote', 0.5, { x: 200, opacity: 0 });
+tl.from('.about-quote', 0.5, { x: 200, opacity: 0 });
 tl.from('.about-span', 1, { width: 0 }, '=-.5');
 tl.from('#office', 1, { x: -200, opacity: 0, ease: Power4.easeInOut }, '=-1');
 tl.from('#building', 1, { x: 200, opacity: 0, ease: Power4.easeInOut }, '=-.7');
 
-tl2.from('#box', 1, { opacity: 0, scale: 0 });
-tl2.to('#box', 0.5, {
+tl2.from('#box1', 1, { opacity: 0, scale: 0 });
+tl2.to('#box1', 0.5, {
   left: '20%',
   scale: 1.3,
   borderColor: 'white',
-  borderWidth: 12,
-  boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)',
+  opacity: 1,
+  borderWidth: 8,
 });
 
 const scene = new ScrollMagic.Scene({
@@ -38,7 +38,7 @@ const scene = new ScrollMagic.Scene({
   .addTo(controller);
 
 const scene2 = new ScrollMagic.Scene({
-  triggerElement: 'blockquote',
+  triggerElement: '.about-quote',
 })
   .setTween(tl2)
   .addTo(controller);
@@ -47,4 +47,45 @@ function updatePercentage() {
   //percent.innerHTML = (tl.progress() *100 ).toFixed();
   tl.progress();
   console.log(tl.progress());
+}
+
+// Product Page
+
+let tlp = new TimelineMax({ onUpdate: updatePercentage });
+let tl2p = new TimelineMax();
+const controllerp = new ScrollMagic.Controller();
+
+tlp.from('.product-quote', 0.5, { x: 200, opacity: 0 });
+tlp.from('.product-span', 1, { width: 0 }, '=-.5');
+tlp.from('#virtual', 1, { x: -200, opacity: 0, ease: Power4.easeInOut }, '=-1');
+tlp.from('#editing', 1, { x: 200, opacity: 0, ease: Power4.easeInOut }, '=-.7');
+
+tl2p.from('#box2', 1, { opacity: 0, scale: 0 });
+tl2p.to('#box2', 0.5, {
+  left: '20%',
+  scale: 1.3,
+  borderColor: 'white',
+  opacity: 1,
+  borderWidth: 8,
+});
+
+const scene3 = new ScrollMagic.Scene({
+  triggerElement: '.product',
+  triggerHook: 'onLeave',
+  duration: '100%',
+})
+  .setPin('.product')
+  .setTween(tlp)
+  .addTo(controller);
+
+const scene4 = new ScrollMagic.Scene({
+  triggerElement: '.product-quote',
+})
+  .setTween(tl2p)
+  .addTo(controllerp);
+
+function updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+  tlp.progress();
+  console.log(tlp.progress());
 }
